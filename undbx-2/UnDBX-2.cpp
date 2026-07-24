dbc6bea2b1b6ff2a99744c51aec1b427681c05bf
beemfx <beem.media@outlook.com>
2026-07-23 22:39:37 -0500

    Infrastructure for creating DB.

--- undbx-2/UnDBX-2.cpp
+++ undbx-2/UnDBX-2.cpp
@@ -34,6 +34,59 @@ namespace UnDBX2
 
 		return filePaths;
 	}
+
+	class UnDBXDatabase
+	{
+	private:
+		struct Message
+		{
+			dbx_type_t Type = DBX_TYPE_UNKNOWN;
+			std::u8string Text;
+			std::size_t TextHash = 0;
+
+			bool operator == (const Message& Other) const
+			{
+				return Type == Other.Type && Text == Other.Text;
+			}
+		};
+
+		std::vector<Message> m_AllMessages;
+
+	public:
+		void AddMessage(dbx_type_t Type, const char* RawText, unsigned int RawTextSize)
+		{
+			if (Type == DBX_TYPE_EMAIL || Type == DBX_TYPE_OE4)
+			{
+				Message msg = { Type, std::u8string(reinterpret_cast<const char8_t*>(RawText)) };
+				msg.TextHash = std::hash<std::u8string>{}(msg.Text);
+
+				// If a duplicate message is found, ignore it.
+				if (!Contains(msg))
+				{
+					m_AllMessages.push_back(msg);
+				}
+			}
+		}
+
+		void PrintStats()
+		{
+			std::cout << "Message Count: " << m_AllMessages.size() << std::endl;
+		}
+
+	private:
+		bool Contains(const Message& Msg)
+		{
+			for (const auto& CmpMsg : m_AllMessages)
+			{
+				if (CmpMsg.TextHash == Msg.TextHash && CmpMsg == Msg)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	};
 }
 
 int main(int argc, char** argv)
@@ -46,6 +99,8 @@ int main(int argc, char** argv)
 	{
 		return -1;
 	}
+
+	UnDBXDatabase db;
 
 	const fs::path DirToSearch = argv[1];
 	const std::vector<fs::path> AllFiles = GetAllFilesRecursive(DirToSearch);
@@ -69,10 +124,12 @@ int main(int argc, char** argv)
 			{
 				unsigned int MessageSize = 0;
 				const char* Message = dbx_message(dbx, i, &MessageSize);
-				MessageSize = MessageSize;
+				db.AddMessage(dbx->type, Message, MessageSize);
 			}
 
 			dbx_close(dbx);
 		}
 	}
+
+	db.PrintStats();
 }
